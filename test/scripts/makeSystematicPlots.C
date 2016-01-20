6d0a1275c1876629a508703bc32c22d99bb3d43b
Justin Pilot <justin.pilot@cern.ch>
2016-01-20 10:07:52 -0600

    Added scripts

---
+++ test/scripts/makeSystematicPlots.C
@@ -0,0 +1,164 @@
+void makeSystPlot(string process, string systematic, string label){
+
+	TFile *inFile = new TFile("templates.root", "READ");
+
+	TH1F *upShape_0btagH = (TH1F *) inFile->Get( Form("btag0__%s__%s__plus", process.c_str(), systematic.c_str()) );
+	TH1F *upShape_1btagH = (TH1F *) inFile->Get( Form("btag1__%s__%s__plus", process.c_str(), systematic.c_str()) );
+	TH1F *upShape_2btagH = (TH1F *) inFile->Get( Form("btag2__%s__%s__plus", process.c_str(), systematic.c_str()) );
+	TH1F *dnShape_0btagH = (TH1F *) inFile->Get( Form("btag0__%s__%s__minus", process.c_str(), systematic.c_str()) );
+	TH1F *dnShape_1btagH = (TH1F *) inFile->Get( Form("btag1__%s__%s__minus", process.c_str(), systematic.c_str()) );
+	TH1F *dnShape_2btagH = (TH1F *) inFile->Get( Form("btag2__%s__%s__minus", process.c_str(), systematic.c_str()) );
+	TH1F *nomShape_0btagH = (TH1F *) inFile->Get( Form("btag0__%s", process.c_str()) );
+	TH1F *nomShape_1btagH = (TH1F *) inFile->Get( Form("btag1__%s", process.c_str()) );
+	TH1F *nomShape_2btagH = (TH1F *) inFile->Get( Form("btag2__%s", process.c_str()) );
+	
+	TH1F *upShape_3btagH = (TH1F *) inFile->Get( Form("btag3__%s__%s__plus", process.c_str(), systematic.c_str()) );
+	TH1F *upShape_4btagH = (TH1F *) inFile->Get( Form("btag4__%s__%s__plus", process.c_str(), systematic.c_str()) );
+	TH1F *upShape_5btagH = (TH1F *) inFile->Get( Form("btag5__%s__%s__plus", process.c_str(), systematic.c_str()) );
+	TH1F *dnShape_3btagH = (TH1F *) inFile->Get( Form("btag3__%s__%s__minus", process.c_str(), systematic.c_str()) );
+	TH1F *dnShape_4btagH = (TH1F *) inFile->Get( Form("btag4__%s__%s__minus", process.c_str(), systematic.c_str()) );
+	TH1F *dnShape_5btagH = (TH1F *) inFile->Get( Form("btag5__%s__%s__minus", process.c_str(), systematic.c_str()) );
+	TH1F *nomShape_3btagH = (TH1F *) inFile->Get( Form("btag3__%s", process.c_str()) );
+	TH1F *nomShape_4btagH = (TH1F *) inFile->Get( Form("btag4__%s", process.c_str()) );
+	TH1F *nomShape_5btagH = (TH1F *) inFile->Get( Form("btag5__%s", process.c_str()) );
+	
+
+
+
+	for (int nTags = 0; nTags < 6; nTags++){
+
+	TH1F *nomShape = new TH1F;
+	TH1F *upShape = new TH1F;
+	TH1F *dnShape = new TH1F;
+
+	string btagLabel = "blah";
+
+	if(nTags == 0){
+		btagLabel = "0 b-tags, #Deltay < 1";
+		nomShape = (TH1F *) nomShape_0btagH;
+		upShape = (TH1F *) upShape_0btagH;
+		dnShape = (TH1F *) dnShape_0btagH;
+	}
+	if(nTags == 1){
+		btagLabel = "1 b-tags, #Deltay < 1";
+		nomShape = nomShape_1btagH;
+		upShape = upShape_1btagH;
+		dnShape = dnShape_1btagH;
+	}
+	if(nTags == 2){
+		btagLabel = "2 b-tags, #Deltay < 1";
+		nomShape = nomShape_2btagH;
+		upShape = upShape_2btagH;
+		dnShape = dnShape_2btagH;
+	}
+	if(nTags == 3){
+		btagLabel = "0 b-tags, #Deltay > 1";
+		nomShape = nomShape_3btagH;
+		upShape = upShape_3btagH;
+		dnShape = dnShape_3btagH;
+	}
+	if(nTags == 4){
+		btagLabel = "1 b-tags, #Deltay > 1";
+		nomShape = nomShape_4btagH;
+		upShape = upShape_4btagH;
+		dnShape = dnShape_4btagH;
+	}
+	if(nTags == 5){
+		btagLabel = "2 b-tags, #Deltay > 1";
+		nomShape = nomShape_5btagH;
+		upShape = upShape_5btagH;
+		dnShape = dnShape_5btagH;
+	}
+	nomShape->Sumw2(); upShape->Sumw2(); dnShape->Sumw2();
+
+	nomShape->SetFillColor(0);
+	nomShape->SetLineColor(kBlack);
+	upShape->SetFillColor(0);
+	dnShape->SetFillColor(0);
+	
+	TCanvas *c1 = new TCanvas("c1", "c1",0,0,600,500);
+	c1->Range(0,0,1,1);
+	c1->Draw();
+
+	TPad *c1_1 = new TPad("c1_1", "newpad",0.01,0.01,0.99,0.32);
+	c1_1->Draw();
+	TPad *c1_2 = new TPad("c1_2", "newpad",0.01,0.33,0.99,0.99);
+	c1_2->Draw(); 
+
+	c1->cd();
+	c1_2->cd();
+	c1_2->SetTopMargin(0.1);
+	c1_2->SetBottomMargin(0.01);
+	c1_2->SetRightMargin(0.05);
+	c1_2->SetLeftMargin(0.1);
+	c1_2->SetFillStyle(0);
+
+
+	nomShape->GetYaxis()->SetTitle("Events");
+	nomShape->GetXaxis()->SetLabelSize(0);
+	nomShape->SetMaximum(TMath::Max(upShape->GetMaximum(), dnShape->GetMaximum())*1.30);
+	nomShape->Draw("hist");
+	upShape->SetLineColor(kGreen + 1);
+	upShape->Draw("hist same");
+	dnShape->SetLineColor(kRed);
+	dnShape->Draw("hist same");
+
+	TLegend *leg = new TLegend(0.6,0.6,0.9,0.88);
+	leg->AddEntry(nomShape, label.c_str(), "");
+	leg->AddEntry(nomShape, "Nominal", "l");
+	leg->AddEntry(upShape, "+1 #sigma", "l");
+	leg->AddEntry(dnShape, "-1 #sigma", "l");
+	leg->AddEntry(nomShape, btagLabel.c_str(), "");
+	leg->SetFillColor(0);
+	leg->SetLineColor(0);
+	leg->Draw();
+
+
+	TLatex *cmsLabel = new TLatex();
+	cmsLabel.SetNDC();
+	//cmsLabel.DrawLatex(0.1, 0.93, "QCD MC");
+	if (systematic == "jec") cmsLabel.DrawLatex(0.6,0.93, "Jet Energy Scale");
+	if (systematic == "pdf") cmsLabel.DrawLatex(0.6,0.93, "PDF");
+	if (systematic == "q2") cmsLabel.DrawLatex(0.6,0.93, "Q^{2} Scale");
+	if (systematic == "btag") cmsLabel.DrawLatex(0.6,0.93, "b-Tagging");
+	if (systematic == "jer") cmsLabel.DrawLatex(0.6,0.93, "Jet Energy Resolution");
+	if (systematic == "misErr") cmsLabel.DrawLatex(0.6,0.93, "NTMJ Estimate");
+
+	c1_1->cd();
+	c1_1->SetTopMargin(0.01);
+	c1_1->SetBottomMargin(0.3);
+	c1_1->SetRightMargin(0.05);
+	c1_1->SetLeftMargin(0.1);
+	c1_1->SetFillStyle(0);
+
+	TH1F *upRatio = (TH1F *) upShape->Clone("upRatio");
+	TH1F *dnRatio = (TH1F *) dnShape->Clone("dnRatio");
+	upRatio->Sumw2();
+	dnRatio->Sumw2();
+	
+
+	upRatio->Divide(nomShape);
+	dnRatio->Divide(nomShape);
+
+	upRatio->GetYaxis()->SetRangeUser(0.,2.5);
+	upRatio->GetYaxis()->SetTitle("Ratio to Nominal");
+	upRatio->GetYaxis()->SetTitleOffset(0.4);
+	upRatio->GetYaxis()->SetTitleSize(0.11);
+	upRatio->GetXaxis()->SetLabelSize(0.11);
+	upRatio->GetXaxis()->SetTitleSize(0.11);
+	upRatio->GetXaxis()->SetTitle( "t#bar{t} Invariant Mass [GeV]");
+
+	upRatio->Draw("hist");
+	dnRatio->Draw("hist same");
+
+	TF1 *line = new TF1("line", "1", 0, 5000);
+	line->SetLineColor(kBlack);
+	line->Draw("same");
+
+
+	c1->SaveAs( Form("Syst_%s_%s_%dbtags.pdf", systematic.c_str(), process.c_str(), nTags) );
+
+
+	}//nTags Loop
+}
+
---
+++ test/scripts/runSystematics.C
@@ -0,0 +1,115 @@
+{
+
+makeSystPlot("Zprime1000", "jec", "Narrow Z' (1 TeV)");
+makeSystPlot("Zprime1500", "jec", "Narrow Z' (1.5 TeV)");
+makeSystPlot("Zprime2000", "jec", "Narrow Z' (2 TeV)");
+makeSystPlot("Zprime2500", "jec", "Narrow Z' (2.5 TeV)");
+makeSystPlot("Zprime3000", "jec", "Narrow Z' (3 TeV)");
+makeSystPlot("Zprime3500", "jec", "Narrow Z' (3.5 TeV)");
+makeSystPlot("Zprime4000", "jec", "Narrow Z' (4 TeV)");
+
+makeSystPlot("Zprime1000", "jer", "Narrow Z' (1 TeV)");
+makeSystPlot("Zprime1500", "jer", "Narrow Z' (1.5 TeV)");
+makeSystPlot("Zprime2000", "jer", "Narrow Z' (2 TeV)");
+makeSystPlot("Zprime2500", "jer", "Narrow Z' (2.5 TeV)");
+makeSystPlot("Zprime3000", "jer", "Narrow Z' (3 TeV)");
+makeSystPlot("Zprime3500", "jer", "Narrow Z' (3.5 TeV)");
+makeSystPlot("Zprime4000", "jer", "Narrow Z' (4 TeV)");
+/*
+makeSystPlot("Zprime1000", "pileup", "Narrow Z' (1 TeV)");
+makeSystPlot("Zprime1500", "pileup", "Narrow Z' (1.5 TeV)");
+makeSystPlot("Zprime2000", "pileup", "Narrow Z' (2 TeV)");
+makeSystPlot("Zprime3000", "pileup", "Narrow Z' (3 TeV)");
+
+makeSystPlot("Zprime1000", "pdf", "Narrow Z' (1 TeV)");
+makeSystPlot("Zprime1500", "pdf", "Narrow Z' (1.5 TeV)");
+makeSystPlot("Zprime2000", "pdf", "Narrow Z' (2 TeV)");
+makeSystPlot("Zprime3000", "pdf", "Narrow Z' (3 TeV)");
+
+makeSystPlot("Zprime1000", "subjbtag", "Narrow Z' (1 TeV)");
+makeSystPlot("Zprime1500", "subjbtag", "Narrow Z' (1.5 TeV)");
+makeSystPlot("Zprime2000", "subjbtag", "Narrow Z' (2 TeV)");
+makeSystPlot("Zprime3000", "subjbtag", "Narrow Z' (3 TeV)");
+
+makeSystPlot("Zprime1000", "jer", "Narrow Z' (1 TeV)");
+makeSystPlot("Zprime1500", "jer", "Narrow Z' (1.5 TeV)");
+makeSystPlot("Zprime2000", "jer", "Narrow Z' (2 TeV)");
+makeSystPlot("Zprime3000", "jer", "Narrow Z' (3 TeV)");
+
+makeSystPlot("ZprimeWide1000", "jec", "Wide Z' (1 TeV)");
+makeSystPlot("ZprimeWide1500", "jec", "Wide Z' (1.5 TeV)");
+makeSystPlot("ZprimeWide2000", "jec", "Wide Z' (2 TeV)");
+makeSystPlot("ZprimeWide3000", "jec", "Wide Z' (3 TeV)");
+
+makeSystPlot("ZprimeWide1000", "pileup", "Wide Z' (1 TeV)");
+makeSystPlot("ZprimeWide1500", "pileup", "Wide Z' (1.5 TeV)");
+makeSystPlot("ZprimeWide2000", "pileup", "Wide Z' (2 TeV)");
+makeSystPlot("ZprimeWide3000", "pileup", "Wide Z' (3 TeV)");
+
+makeSystPlot("ZprimeWide1000", "subjbtag", "Wide Z' (1 TeV)");
+makeSystPlot("ZprimeWide1500", "subjbtag", "Wide Z' (1.5 TeV)");
+makeSystPlot("ZprimeWide2000", "subjbtag", "Wide Z' (2 TeV)");
+makeSystPlot("ZprimeWide3000", "subjbtag", "Wide Z' (3 TeV)");
+
+makeSystPlot("ZprimeWide1000", "jer", "Wide Z' (1 TeV)");
+makeSystPlot("ZprimeWide1500", "jer", "Wide Z' (1.5 TeV)");
+makeSystPlot("ZprimeWide2000", "jer", "Wide Z' (2 TeV)");
+makeSystPlot("ZprimeWide3000", "jer", "Wide Z' (3 TeV)");
+
+makeSystPlot("ZprimeWide1000", "pdf", "Wide Z' (1 TeV)");
+makeSystPlot("ZprimeWide1500", "pdf", "Wide Z' (1.5 TeV)");
+makeSystPlot("ZprimeWide2000", "pdf", "Wide Z' (2 TeV)");
+makeSystPlot("ZprimeWide3000", "pdf", "Wide Z' (3 TeV)");
+
+makeSystPlot("RSgluon1000", "jec", "RS Gluon (1 TeV)");
+makeSystPlot("RSgluon1500", "jec", "RS Gluon (1.5 TeV)");
+makeSystPlot("RSgluon2000", "jec", "RS Gluon (2 TeV)");
+makeSystPlot("RSgluon3000", "jec", "RS Gluon (3 TeV)");
+makeSystPlot("RSgluon1400", "jec", "RS Gluon (1.4 TeV)");
+makeSystPlot("RSgluon1800", "jec", "RS Gluon (1.8 TeV)");
+makeSystPlot("RSgluon2500", "jec", "RS Gluon (2.5 TeV)");
+
+makeSystPlot("RSgluon1000", "jer", "RS Gluon (1 TeV)");
+makeSystPlot("RSgluon1500", "jer", "RS Gluon (1.5 TeV)");
+makeSystPlot("RSgluon2000", "jer", "RS Gluon (2 TeV)");
+makeSystPlot("RSgluon3000", "jer", "RS Gluon (3 TeV)");
+makeSystPlot("RSgluon1400", "jer", "RS Gluon (1.4 TeV)");
+makeSystPlot("RSgluon1800", "jer", "RS Gluon (1.8 TeV)");
+makeSystPlot("RSgluon2500", "jer", "RS Gluon (2.5 TeV)");
+
+makeSystPlot("RSgluon1000", "pileup", "RS Gluon (1 TeV)");
+makeSystPlot("RSgluon1500", "pileup", "RS Gluon (1.5 TeV)");
+makeSystPlot("RSgluon2000", "pileup", "RS Gluon (2 TeV)");
+makeSystPlot("RSgluon3000", "pileup", "RS Gluon (3 TeV)");
+makeSystPlot("RSgluon1400", "pileup", "RS Gluon (1.4 TeV)");
+makeSystPlot("RSgluon1800", "pileup", "RS Gluon (1.8 TeV)");
+makeSystPlot("RSgluon2500", "pileup", "RS Gluon (2.5 TeV)");
+
+makeSystPlot("RSgluon1000", "pdf", "RS Gluon (1 TeV)");
+makeSystPlot("RSgluon1500", "pdf", "RS Gluon (1.5 TeV)");
+makeSystPlot("RSgluon2000", "pdf", "RS Gluon (2 TeV)");
+makeSystPlot("RSgluon3000", "pdf", "RS Gluon (3 TeV)");
+makeSystPlot("RSgluon1400", "pdf", "RS Gluon (1.4 TeV)");
+makeSystPlot("RSgluon1800", "pdf", "RS Gluon (1.8 TeV)");
+makeSystPlot("RSgluon2500", "pdf", "RS Gluon (2.5 TeV)");
+
+makeSystPlot("RSgluon1000", "subjbtag", "RS Gluon (1 TeV)");
+makeSystPlot("RSgluon1500", "subjbtag", "RS Gluon (1.5 TeV)");
+makeSystPlot("RSgluon2000", "subjbtag", "RS Gluon (2 TeV)");
+makeSystPlot("RSgluon3000", "subjbtag", "RS Gluon (3 TeV)");
+makeSystPlot("RSgluon1400", "subjbtag", "RS Gluon (1.4 TeV)");
+makeSystPlot("RSgluon1800", "subjbtag", "RS Gluon (1.8 TeV)");
+makeSystPlot("RSgluon2500", "subjbtag", "RS Gluon (2.5 TeV)");
+
+makeSystPlot("qcd", "misErr", "QCD");
+*/
+makeSystPlot("ttbar", "jec", "t#bar{t}");
+makeSystPlot("ttbar", "jer", "t#bar{t}");
+//makeSystPlot("ttbar", "q2", "t#bar{t}");
+//makeSystPlot("ttbar", "subjbtag", "t#bar{t}");
+//makeSystPlot("ttbar", "pdf", "t#bar{t}");
+
+}
+
+
+
