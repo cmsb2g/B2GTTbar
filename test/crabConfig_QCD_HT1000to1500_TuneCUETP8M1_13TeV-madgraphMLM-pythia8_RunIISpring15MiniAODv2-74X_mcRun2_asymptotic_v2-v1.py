663e8f583c6af28aed15c6a1e95d66bb76c19ad0
Christine <camclean@ucdavis.edu>
2016-06-30 08:58:30 -0500

    Updated tree-making to do pileup reweighting based on 72 mb minbias xs

--- test/crabConfig_QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1.py
+++ test/crabConfig_QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1.py
@@ -2,14 +2,14 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'QCDAnaTrees_QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_noTrigger_20160318'
+config.General.requestName = 'QCDAnaTrees_QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_noTrigger_20160630Combo'
 config.General.workArea = 'QCDAnaRunII'
 config.General.transferLogs = True
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'PSet.py'
-config.JobType.inputFiles = ['FrameworkJobReport.xml', 'execute_for_crab_qcd.py', 'NtupleReader_fwlite.py', 'leptonic_nu_z_component.py', 'JECs', 'ModMass_2015_09_22.root', 'MistagRate_2015_09_25.root', 'PUweight20160316.root', 'MyDataPileupHistogram.root', 'MyDataPileupHistogramUP.root', 'MyDataPileupHistogramDN.root']
+config.JobType.inputFiles = ['FrameworkJobReport.xml', 'execute_for_crab_qcd.py', 'NtupleReader_fwlite.py', 'leptonic_nu_z_component.py', 'JECs', 'ModMass_2015_09_22.root', 'MistagRate_2015_09_25.root', 'PUweight20160316.root', 'MyDataPileupHistogram_72mb2015.root', 'MyDataPileupHistogramUP_75p6mb2015.root', 'MyDataPileupHistogramDN_68p4mb2015.root']
 config.JobType.outputFiles = ['outplots.root']
 config.JobType.scriptExe = 'execute_for_crab_qcd.sh'
 
