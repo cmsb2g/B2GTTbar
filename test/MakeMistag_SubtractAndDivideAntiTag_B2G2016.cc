84c7f3e72686481b351c781a577988a6d30e38af
James <jdolen@fnal.gov>
2016-10-10 16:57:47 -0500

    addQCDscale. add new makemistag

---
+++ test/MakeMistag_SubtractAndDivideAntiTag_B2G2016.cc
@@ -0,0 +1,421 @@
+// MakeMistag_SubtractAndDivideAntiTag.cc
+// Divide two histograms to calculate mistag rate or efficiency
+// [] .L MakeMistag_SubtractAndDivideAntiTag.cc+
+// [] run()
+
+#include "TROOT.h"
+#include "TStyle.h"
+#include "TLatex.h"
+#include "TFile.h"
+#include "TCanvas.h"
+#include "TLegend.h"
+#include "TEfficiency.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TF1.h"
+#include "TMath.h"
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+void makeHists(bool, bool, vector<double>);
+
+void run(){
+
+  // Define mistag rate bins
+  vector <double> bins_small;
+  vector <double> bins_0btag;
+  vector <double> bins_1btag;
+  vector <double> bins_2btag;
+  vector <double> bins_big;
+  vector <double> bins_eta;
+  int x;
+
+  // Small bins (for visual not for bkgd est)
+  cout<<"Define small bins: ";
+  x=400;
+  while (x<=7000){
+    cout<<" "<<x;
+    bins_small.push_back(x);
+    if ( x<1000) x+=50;
+    else if (x<2000) x+=100;
+    else if (x<4000) x+=500;
+    else x+=1000;
+  }
+  cout<<endl;
+
+  //0 btag
+  cout<<"Define 0btag bins: ";
+  x=400;
+  while (x<=6000){
+    cout<<" "<<x;
+    bins_0btag.push_back(x);
+    if ( x<1000) x+=100;
+    else if (x<1200) x+=200;
+    else if (x<1500) x+=300;
+    else if (x<2000) x+=500;
+    else if (x<5000) x+=1000;
+    else x+=1000;
+  }
+  cout<<endl;
+
+  // 1 btag
+  cout<<"Define 1btag bins: ";
+  x=400;
+  while (x<=5000){
+    cout<<" "<<x;
+    bins_1btag.push_back(x);
+    if ( x<600) x+=100;
+    else if (x<1000) x+=200;
+    else if (x<1400) x+=400;
+    else if (x<2000) x+=600;
+    else if (x<3000) x+=1000;
+    else x+=2000;
+  }
+  cout<<endl;
+
+  // 2 btag
+  cout<<"Define 2btag bins: ";
+  x=400;
+  while (x<=6000){
+    cout<<" "<<x;
+    bins_2btag.push_back(x);
+    if ( x<600) x+=200;
+    else if (x<5000) x+=1000;
+    else x+=1000;
+  }
+  cout<<endl;
+
+  // big bins
+  cout<<"Define big bins: ";
+  x=400;
+  while (x<=8000){
+    cout<<" "<<x;
+    bins_big.push_back(x);
+    if ( x<1000) x+=100;
+    else x+=6000;
+    // else if (x<2000) x+=200;
+    // else if (x<3000) x+=500;
+    // else if (x<5000) x+=1000;
+    // else x+=2000;
+  }
+  cout<<endl;
+
+  // eta bins
+  cout<<"Define eta bins: ";
+  for (double i = -2.4; i<= 2.4; i+=0.2){
+      bins_eta.push_back(i);
+      cout<<" "<<i;
+  }
+  cout<<endl;
+
+
+  // bool data, bool noSubtract, vector of bins            makeHists( bool data, bool noSubtract, vector<double> bins ){
+  makeHists(false, true, bins_small );  // Monte Carlo  - small bins
+  makeHists(false, true, bins_0btag );  // Monte Carlo  - bins_medium
+  makeHists(false, true, bins_1btag );  // Monte Carlo  - bins_medium
+  makeHists(false, true, bins_2btag );  // Monte Carlo  - bins_medium
+
+  // makeHists(true, false, bins_small );  // data, subtract ttbar  - small bins
+  // makeHists(true, false, bins_0btag );  // data, subtract ttbar  - bins_medium
+  // makeHists(true, false, bins_1btag );  // data, subtract ttbar  - bins_medium
+  // makeHists(true, false, bins_2btag );  // data, subtract ttbar  - bins_medium
+
+}
+
+
+// void makeHists( bool data = false, bool noSubtract = false, vector<int> bins ){
+void makeHists( bool data, bool noSubtract, vector<double> bins ){
+	// Get input file and setup output file
+  TFile * InFile;
+  TFile * InFileTTbar;
+  TFile * OutFileBigBins;
+  TFile * OutFile;
+
+  string infile_name ;
+  string infile_ttbar_name ;
+
+  // QCD Monte Carlo Input (scaled) and Output files
+  if (!data) infile_name = "histsAllHad_Sept19_b2gtree_QCD_Pt_300toInf_pythia8_RunIISpring16MiniAODv2_reHLT_V2.root";
+
+  if (data){
+
+   // infile_name       = "outAntiTag_JetHT_BothParts_B2GAnaFW_v74x_V8p4_25ns_Nov13silverJSON_reader5a85e65_030516.root";
+   // infile_ttbar_name = "outAntiTag_TT_TuneCUETP8M1_13TeV-powheg-pythia8_janos_B2Gv8p4_reader603e_030716.root";
+      infile_name       = "histsAllHad_Sept19_b2gtree_JetHT_combined.root";
+      infile_ttbar_name = "histsAllHad_Sept19_b2gtree_TT_TuneCUETP8M1_13TeV-powheg-pythia8_RunIISpring16MiniAODv2-PUSpring16_reHLT_V2_99percentFinished_All.root";
+  } 
+
+  
+
+  string date = "092516";
+  //string syst = "jec_up";
+  
+  
+  int nbins = bins.size();
+  stringstream temp1;
+  temp1 << nbins;
+  string snbins = temp1 .str();
+  string subtractOrNot = "";
+  if (noSubtract) subtractOrNot = "_noSubstract_";
+  if (!noSubtract) subtractOrNot = "_Substract_";
+  if (!data) subtractOrNot = "_MC_";
+
+  string outfile_name ;
+  if (data)  outfile_name = "MistagRate_nbins_"+date+"_"+snbins+"_ttbar"+subtractOrNot+infile_name;
+  if (!data) outfile_name = "MistagRate_nbins_"+date+"_"+snbins+subtractOrNot+infile_name;
+
+  cout<<"Opening "<<infile_name<<endl;
+  InFile        = new TFile(      infile_name.c_str()      );
+  OutFile       = new TFile(     outfile_name.c_str()        , "RECREATE");
+  if (data) InFileTTbar   = new TFile(infile_ttbar_name.c_str()     );
+
+  OutFile->cd();
+
+  // Loop over anti-tag and tag combinations and make mistag rate
+  vector <string> antitag_def;
+  vector <string> tag_def;
+
+  //antitag_def.push_back("AntiTagMinMass30_ReqTopMassFat");
+  //antitag_def.push_back("AntiTagMinMass30_ReqTopMassSD");
+  //antitag_def.push_back("AntiTagMinMass50_ReqTopMassFat");
+  //antitag_def.push_back("AntiTagMinMass50_ReqTopMassSD");
+  //antitag_def.push_back("AntiTagTau32_ReqTopMassFat");
+  antitag_def.push_back("AntiTagTau32_ReqTopMassSD");
+  // antitag_def.push_back("AntiTagMinMass_ReqTopMassSD");
+  // antitag_def.push_back("NoAntiTag_ReqTopMassSD");
+  // antitag_def.push_back("AntiTagTau32_NoMassReq");
+  // antitag_def.push_back("TagOdd");
+  // antitag_def.push_back("TagEven");
+
+ 
+  //tag_def.push_back("_TagMassFatMinMass");
+  // tag_def.push_back("_TagMassSD"        );
+  // tag_def.push_back("_TagTau32"        );
+  tag_def.push_back("_TagMassSDTau32"   );
+  //tag_def.push_back("_TagMassSDMinMass" );
+
+  //string pre = "AllHad/h_";
+  string pre = "h_";
+  string save_pre = "h_mistag_";
+  string probe = "_Probe";
+  vector <string> post;
+  // post.push_back("_jetPt_dRapIn_inclusive");
+  // post.push_back("_jetPt_dRapIn_0btag");
+  // post.push_back("_jetPt_dRapIn_1btag");
+  // post.push_back("_jetPt_dRapIn_2btag");
+  // post.push_back("_jetPt_dRapHi_inclusive");
+  // post.push_back("_jetPt_dRapHi_0btag");
+  // post.push_back("_jetPt_dRapHi_1btag");
+  // post.push_back("_jetPt_dRapHi_2btag");
+  // post.push_back("_jetPt_dRapLo_inclusive");
+  // post.push_back("_jetPt_dRapLo_0btag");
+  // post.push_back("_jetPt_dRapLo_1btag");
+  // post.push_back("_jetPt_dRapLo_2btag");
+  
+  post.push_back("_jetP_dRapIn_inclusive");
+  post.push_back("_jetP_dRapIn_0btag");
+  post.push_back("_jetP_dRapIn_1btag");
+  post.push_back("_jetP_dRapIn_2btag");
+  post.push_back("_jetP_dRapHi_inclusive");
+  post.push_back("_jetP_dRapHi_0btag");
+  post.push_back("_jetP_dRapHi_1btag");
+  post.push_back("_jetP_dRapHi_2btag");
+  post.push_back("_jetP_dRapLo_inclusive");
+  post.push_back("_jetP_dRapLo_0btag");
+  post.push_back("_jetP_dRapLo_1btag");
+  post.push_back("_jetP_dRapLo_2btag");
+
+  // post.push_back("_jetRap_dRapIn_inclusive");
+  // post.push_back("_jetRap_dRapIn_0btag");
+  // post.push_back("_jetRap_dRapIn_1btag");
+  // post.push_back("_jetRap_dRapIn_2btag");
+  // post.push_back("_jetRap_dRapHi_inclusive");
+  // post.push_back("_jetRap_dRapHi_0btag");
+  // post.push_back("_jetRap_dRapHi_1btag");
+  // post.push_back("_jetRap_dRapHi_2btag");
+  // post.push_back("_jetRap_dRapLo_inclusive");
+  // post.push_back("_jetRap_dRapLo_0btag");
+  // post.push_back("_jetRap_dRapLo_1btag");
+  // post.push_back("_jetRap_dRapLo_2btag");
+
+
+
+
+  // post.push_back("_2D_dRapAll_inclusive");
+  // post.push_back("_2D_dRapAll_0btag");
+  // post.push_back("_2D_dRapAll_1btag");
+  // post.push_back("_2D_dRapAll_2btag");
+  // post.push_back("_2D_dRapHi_inclusive");
+  // post.push_back("_2D_dRapHi_0btag");
+  // post.push_back("_2D_dRapHi_1btag");
+  // post.push_back("_2D_dRapHi_2btag");
+  // post.push_back("_2D_dRapLo_inclusive");
+  // post.push_back("_2D_dRapLo_0btag");
+  // post.push_back("_2D_dRapLo_1btag");
+  // post.push_back("_2D_dRapLo_2btag");
+
+  // post.push_back("_jetPt_dRapHi_inclusive");
+  // post.push_back("_jetPt_dRapHi_2btag");
+  // post.push_back("_jetPt_dRapHi_1btag");
+  // post.push_back("_jetPt_dRapHi_0btag");
+  // post.push_back("_jetPt_dRapLo_inclusive");
+  // post.push_back("_jetPt_dRapLo_2btag");
+  // post.push_back("_jetPt_dRapLo_1btag");
+  // post.push_back("_jetPt_dRapLo_0btag");
+  
+  
+  
+  for (unsigned int i=0; i<antitag_def.size(); i++ ){
+    for (unsigned int j=0; j<tag_def.size(); j++ ){
+      for (unsigned int k=0; k<post.size(); k++ ){
+        string numer = pre + antitag_def[i] + tag_def[j] + post[k] ;
+        cout <<"numer:"<<numer<<endl;
+        string denom = pre + antitag_def[i] + probe + post[k] ;
+        cout <<"denom:"<<denom<<endl;
+        string savename = save_pre + antitag_def[i] + tag_def[j] + post[k] ;
+        string savename2 = save_pre + antitag_def[i] + tag_def[j] + post[k] ;
+        string savename3 = "teff_" + antitag_def[i] + tag_def[j] + post[k] ;
+
+        // cout<<numer<<"      "<<denom<<"     "<<
+        cout<<savename<<endl;
+
+        TH1D * bkgd_numer = (TH1D*)InFile->Get(numer.c_str());
+        TH1D * bkgd_denom = (TH1D*)InFile->Get(denom.c_str());
+        bkgd_numer  ->Sumw2();
+        bkgd_denom  ->Sumw2();
+        
+        TH1D *bkgd_numer_new =(TH1D*) bkgd_numer->Clone();
+        TH1D *bkgd_denom_new =(TH1D*) bkgd_denom->Clone();
+        bkgd_numer_new->Sumw2();
+        bkgd_denom_new->Sumw2();
+
+        // rebin with variable width bins
+        int nbins = bins.size();
+        double xbins[nbins];
+        for( unsigned int i = 0; i < bins.size(); i++) xbins[i] = bins[i];
+        for( unsigned int i = 0; i < bins.size(); i++) cout<<xbins[i]<<" ";
+        cout<<endl;
+        cout<<"nbins "<<nbins<<endl;
+        TH1D* bkgd_numer_rebin = (TH1D*)bkgd_numer_new->Rebin(nbins-1,"bkgd_numer_rebin",xbins);
+        TH1D* bkgd_denom_rebin = (TH1D*)bkgd_denom_new->Rebin(nbins-1,"bkgd_denom_rebin",xbins);
+        bkgd_numer_rebin->Sumw2(); 
+        bkgd_denom_rebin->Sumw2();
+
+        TH1D *ttbar_numer_rebin;
+        TH1D *ttbar_denom_rebin;
+        if (data){
+
+          TH1D * ttbar_numer = (TH1D*)InFileTTbar->Get(numer.c_str());
+          TH1D * ttbar_denom = (TH1D*)InFileTTbar->Get(denom.c_str());
+          ttbar_numer ->Sumw2();
+          ttbar_denom ->Sumw2();
+          TH1D *ttbar_numer_new;
+          TH1D *ttbar_denom_new;
+          ttbar_numer_new =(TH1D*) ttbar_numer->Clone();
+          ttbar_denom_new =(TH1D*) ttbar_denom->Clone();
+          ttbar_numer_new->Sumw2();
+          ttbar_denom_new->Sumw2(); 
+          ttbar_numer_rebin = (TH1D*)ttbar_numer_new->Rebin(nbins-1,"ttbar_numer_rebin",xbins);
+          ttbar_denom_rebin = (TH1D*)ttbar_denom_new->Rebin(nbins-1,"ttbar_denom_rebin",xbins);
+          ttbar_numer_rebin ->Sumw2();
+          ttbar_denom_rebin ->Sumw2(); 
+        }
+
+        // subtract ttbar contribution
+        if (data && !noSubtract){
+
+          
+          double luminosity =  24570;  //2530;   //1263.890;  //166; pb-1
+          double nevents_dataset_ttbar  = 96834559;
+          double xsec_ttbar  =  831.76  ;
+          double kfactor = 0.93;
+          double toptagsf = 0.82;
+          double scale_ttbar= toptagsf * toptagsf * kfactor * xsec_ttbar * luminosity / nevents_dataset_ttbar;
+          cout<<"scale_ttbar "<<scale_ttbar<<endl;
+          ttbar_numer_rebin->Scale(scale_ttbar);
+          ttbar_denom_rebin->Scale(scale_ttbar);
+
+          string ttbar_numer_name = "check_"+antitag_def[i] + tag_def[j] + post[k] +"_ttbar";
+          string ttbar_denom_name = "check_"+antitag_def[i] + probe      + post[k] +"_ttbar";
+          ttbar_numer_rebin ->SetName(ttbar_numer_name.c_str() );
+          ttbar_denom_rebin ->SetName(ttbar_denom_name.c_str() );
+          ttbar_numer_rebin ->Write();
+          ttbar_denom_rebin ->Write();
+
+
+          string data_nosubtract_numer_name =  "check_"+antitag_def[i] + tag_def[j] + post[k] +"_data_nosubtract";
+          string data_nosubtract_denom_name =  "check_"+antitag_def[i] + probe      + post[k] +"_data_nosubtract";
+          bkgd_numer_rebin ->SetName(data_nosubtract_numer_name.c_str() );
+          bkgd_denom_rebin ->SetName(data_nosubtract_denom_name.c_str() );
+          bkgd_numer_rebin ->Write();
+          bkgd_denom_rebin ->Write();
+
+          cout<<"pre subtract numer "<<bkgd_numer_rebin->Integral()<<endl;
+          cout<<"pre subtract denom "<<bkgd_denom_rebin->Integral()<<endl;
+          bkgd_numer_rebin->Add(ttbar_numer_rebin,-1);
+          bkgd_denom_rebin->Add(ttbar_denom_rebin,-1);
+          cout<<"post subtract numer "<<bkgd_numer_rebin->Integral()<<endl;
+          cout<<"post subtract denom "<<bkgd_denom_rebin->Integral()<<endl;
+
+          string data_subtract_numer_name =  "check_"+antitag_def[i] + tag_def[j] + post[k] +"_data_subtract";
+          string data_subtract_denom_name =  "check_"+antitag_def[i] + probe      + post[k] +"_data_subtract";
+          bkgd_numer_rebin ->SetName(data_subtract_numer_name.c_str() );
+          bkgd_denom_rebin ->SetName(data_subtract_denom_name.c_str() );
+          bkgd_numer_rebin ->Write();
+          bkgd_denom_rebin ->Write();
+        }
+        cout<<"debug1"<<endl;
+
+        // divide histograms
+
+        // TEfficiency * eff_default = new TEfficiency(*bkgd_numer_rebin,*bkgd_denom_rebin);
+        // TEfficiency * eff_normal  = new TEfficiency(*eff_default);
+        // TEfficiency * eff_wilson  = new TEfficiency(*eff_default);
+        // TEfficiency * eff_jeffrey = new TEfficiency(*eff_default);
+        // eff_normal ->SetStatisticOption(TEfficiency::kFNormal);
+        // eff_wilson ->SetStatisticOption(TEfficiency::kFWilson);
+        // eff_jeffrey->SetStatisticOption(TEfficiency::kBJeffrey);
+
+
+        // string savename_eff_default = savename3 + "_default" ;     
+        // string savename_eff_normal  = savename3 + "_normal"  ;    
+        // string savename_eff_wilson  = savename3 + "_wilson"  ;    
+        // string savename_eff_jeffrey = savename3 + "_jeffrey" ;     
+        // eff_default ->SetName(savename_eff_default.c_str());      
+        // eff_normal  ->SetName(savename_eff_normal .c_str());      
+        // eff_wilson  ->SetName(savename_eff_wilson .c_str());
+        // eff_jeffrey ->SetName(savename_eff_jeffrey.c_str());
+        // eff_default ->Write();
+        // eff_normal  ->Write();
+        // eff_wilson  ->Write();
+        // eff_jeffrey ->Write();
+
+        bkgd_numer_rebin->Divide(bkgd_numer_rebin,bkgd_denom_rebin,1,1,"B");
+        bkgd_numer_rebin->SetTitle(";Jet PT (GeV/c);Top Mistag Rate");
+        bkgd_numer_rebin->SetName(savename.c_str());
+        bkgd_numer_rebin->Write();
+
+        // TCanvas* c;
+        // c = new TCanvas("c", "" , 700, 625);
+        // c->cd();
+        // bkgd_numer_rebin->Draw();
+        // bkgd_numer_rebin->GetXaxis()->SetTitleSize(0.03);
+        // bkgd_numer_rebin->GetXaxis()->SetLabelSize(0.025);
+        // bkgd_numer_rebin->GetYaxis()->SetTitleSize(0.03);
+        // bkgd_numer_rebin->GetYaxis()->SetLabelSize(0.025);
+        // bkgd_numer_rebin->GetXaxis()->SetTitleOffset(1.3);
+        // bkgd_numer_rebin->GetYaxis()->SetTitleOffset(1.5);
+        // bkgd_numer_rebin->Draw("SAME");
+        // string savename_mistag = savename +".png";
+        // c->SaveAs(savename_mistag.c_str());
+
+      }
+    }
+  }
+  OutFile->cd();
+  // OutFile->Write();
+  OutFile->Close();
+}
---
+++ test/scaleQCD_PtBinned.C
@@ -0,0 +1,284 @@
+#include "TROOT.h"
+#include "TStyle.h"
+#include "TLatex.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TChain.h"
+#include "TBranch.h"
+#include "TLeaf.h"
+#include "TCanvas.h"
+#include "TLegend.h"
+#include "TH1.h"
+#include "THStack.h"
+#include "TH2.h"
+#include "TF1.h" 
+#include "TProfile.h"
+#include "TProfile2D.h"
+#include "TMath.h"
+#include <TStyle.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+void run(){
+    gROOT->SetBatch(); 
+
+    vector<string> hists_names;
+
+	// hists_names.push_back("mttPredDist2_modMass_tagMassSDTau32_dRapHi_0btag_pred");
+hists_names.push_back("h_DijetMass_dRapIn");  
+hists_names.push_back("h_DijetMass_dRapLo");  
+hists_names.push_back("h_DijetMass_dRapHi");  
+hists_names.push_back("h_2ttag_inclu_dRapIn_DijetMass");  
+hists_names.push_back("h_2ttag_0btag_dRapIn_DijetMass");  
+hists_names.push_back("h_2ttag_1btag_dRapIn_DijetMass");  
+hists_names.push_back("h_2ttag_2btag_dRapIn_DijetMass");  
+hists_names.push_back("h_2ttag_inclu_dRapLo_DijetMass");  
+hists_names.push_back("h_2ttag_0btag_dRapLo_DijetMass");  
+hists_names.push_back("h_2ttag_1btag_dRapLo_DijetMass");  
+hists_names.push_back("h_2ttag_2btag_dRapLo_DijetMass");  
+hists_names.push_back("h_2ttag_inclu_dRapHi_DijetMass");  
+hists_names.push_back("h_2ttag_0btag_dRapHi_DijetMass");  
+hists_names.push_back("h_2ttag_1btag_dRapHi_DijetMass");  
+hists_names.push_back("h_2ttag_2btag_dRapHi_DijetMass");  
+hists_names.push_back("h_DeltaRap");          
+hists_names.push_back("h_DeltaPhi");          
+hists_names.push_back("h_HT");                
+hists_names.push_back("h_MET");               
+hists_names.push_back("h_METphi");            
+hists_names.push_back("h_Nvtx");              
+hists_names.push_back("h_Jet0SDmass");                                   
+hists_names.push_back("h_Jet0SDmass_JetTag_b");                             
+hists_names.push_back("h_Jet0SDmass_JetTag_tau32");                         
+hists_names.push_back("h_Jet0SDmass_JetTag_b_tau32");                       
+hists_names.push_back("h_Jet0SDmass_OppositeJetTag_t_b");                   
+hists_names.push_back("h_Jet0SDmass_OppositeJetTag_t_b_JetTag_b");          
+hists_names.push_back("h_Jet0SDmass_OppositeJetTag_t_b_JetTag_b_tau32");    
+hists_names.push_back("h_JetSDmass");                                   
+hists_names.push_back("h_JetSDmass_JetTag_b");                             
+hists_names.push_back("h_JetSDmass_JetTag_tau32");                         
+hists_names.push_back("h_JetSDmass_JetTag_b_tau32");                       
+hists_names.push_back("h_JetSDmass_OppositeJetTag_t_b");                   
+hists_names.push_back("h_JetSDmass_OppositeJetTag_t_b_JetTag_b");          
+hists_names.push_back("h_JetSDmass_OppositeJetTag_t_b_JetTag_b_tau32"); 
+
+hists_names.push_back("h_JetTau32");                                 
+hists_names.push_back("h_JetTau32_JetTag_b");                        
+hists_names.push_back("h_JetTau32_JetTag_mass");                     
+hists_names.push_back("h_JetTau32_JetTag_b_mass");                   
+hists_names.push_back("h_JetTau32_OppositeJetTag_t_b");              
+hists_names.push_back("h_JetTau32_OppositeJetTag_t_b_JetTag_b");     
+hists_names.push_back("h_JetTau32_OppositeJetTag_t_b_JetTag_b_mass");
+
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_Probe_jetP_dRapIn_inclusive");            
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_Probe_jetP_dRapIn_0btag");                
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_Probe_jetP_dRapIn_1btag");                
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_Probe_jetP_dRapIn_2btag");                
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_Probe_jetP_dRapHi_inclusive");            
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_Probe_jetP_dRapHi_0btag");                
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_Probe_jetP_dRapHi_1btag");                
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_Probe_jetP_dRapHi_2btag");                
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_Probe_jetP_dRapLo_inclusive");            
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_Probe_jetP_dRapLo_0btag");                
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_Probe_jetP_dRapLo_1btag");                
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_Probe_jetP_dRapLo_2btag");                
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_TagMassSDTau32_jetP_dRapIn_inclusive");   
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_TagMassSDTau32_jetP_dRapIn_0btag");       
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_TagMassSDTau32_jetP_dRapIn_1btag");       
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_TagMassSDTau32_jetP_dRapIn_2btag");       
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_TagMassSDTau32_jetP_dRapHi_inclusive");   
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_TagMassSDTau32_jetP_dRapHi_0btag");       
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_TagMassSDTau32_jetP_dRapHi_1btag");       
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_TagMassSDTau32_jetP_dRapHi_2btag");       
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_TagMassSDTau32_jetP_dRapLo_inclusive");   
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_TagMassSDTau32_jetP_dRapLo_0btag");       
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_TagMassSDTau32_jetP_dRapLo_1btag");       
+hists_names.push_back("h_AntiTagTau32_ReqTopMassSD_TagMassSDTau32_jetP_dRapLo_2btag");       
+
+
+
+    TFile * OutFile = new TFile("histsAllHad_Sept19_b2gtree_QCD_Pt_300toInf_pythia8_RunIISpring16MiniAODv2_reHLT_V2.root" , "RECREATE");
+	
+	TFile * fQCD1  = new TFile("histsAllHad_Sept19_b2gtree_QCD_Pt_300to470_pythia8_RunIISpring16MiniAODv2_reHLT_V2.root"   );
+	TFile * fQCD2  = new TFile("histsAllHad_Sept19_b2gtree_QCD_Pt_470to600_pythia8_RunIISpring16MiniAODv2_reHLT_V2.root"   );
+	TFile * fQCD3  = new TFile("histsAllHad_Sept19_b2gtree_QCD_Pt_600to800_pythia8_RunIISpring16MiniAODv2_reHLT_V2.root"   );
+	TFile * fQCD4  = new TFile("histsAllHad_Sept19_b2gtree_QCD_Pt_800to1000_pythia8_RunIISpring16MiniAODv2_reHLT_V2.root"  );
+	TFile * fQCD5  = new TFile("histsAllHad_Sept19_b2gtree_QCD_Pt_1000to1400_pythia8_RunIISpring16MiniAODv2_reHLT_V2.root" );
+	TFile * fQCD6  = new TFile("histsAllHad_Sept19_b2gtree_QCD_Pt_1400to1800_pythia8_RunIISpring16MiniAODv2_reHLT_V2.root" );
+	TFile * fQCD7  = new TFile("histsAllHad_Sept19_b2gtree_QCD_Pt_1800to2400_pythia8_RunIISpring16MiniAODv2_reHLT_V2.root" );
+	TFile * fQCD8  = new TFile("histsAllHad_Sept19_b2gtree_QCD_Pt_2400to3200_pythia8_RunIISpring16MiniAODv2_reHLT_V2.root" );
+	TFile * fQCD9  = new TFile("histsAllHad_Sept19_b2gtree_QCD_Pt_3200toInf_pythia8_RunIISpring16MiniAODv2_reHLT_V2.root"  );
+	
+	// TFile * fQCD1  = new TFile("QCD_HT700_Kinematic_060716_nom_pt400.0.root" );
+	// TFile * fQCD2  = new TFile("QCD_HT1000_Kinematic_060716_nom_pt400.0.root");
+	// TFile * fQCD3  = new TFile("QCD_HT1500_Kinematic_060716_nom_pt400.0.root");
+	// TFile * fQCD4  = new TFile("QCD_HT2000_Kinematic_060716_nom_pt400.0.root");
+
+	OutFile->cd();
+	for (unsigned int i=0; i< hists_names.size(); i++ ){
+		TH1D * h1 = (TH1D*) fQCD1 ->Get( hists_names[i].c_str() ); 
+		TH1D * h2 = (TH1D*) fQCD2 ->Get( hists_names[i].c_str() ); 
+		TH1D * h3 = (TH1D*) fQCD3 ->Get( hists_names[i].c_str() ); 
+		TH1D * h4 = (TH1D*) fQCD4 ->Get( hists_names[i].c_str() ); 
+		TH1D * h5 = (TH1D*) fQCD5 ->Get( hists_names[i].c_str() ); 
+		TH1D * h6 = (TH1D*) fQCD6 ->Get( hists_names[i].c_str() ); 
+		TH1D * h7 = (TH1D*) fQCD7 ->Get( hists_names[i].c_str() ); 
+		TH1D * h8 = (TH1D*) fQCD8 ->Get( hists_names[i].c_str() ); 
+		TH1D * h9 = (TH1D*) fQCD9 ->Get( hists_names[i].c_str() ); 
+
+		cout<<"h1 "<<h1->Integral()<<endl;
+		cout<<"h2 "<<h2->Integral()<<endl;
+		cout<<"h3 "<<h3->Integral()<<endl;
+		cout<<"h4 "<<h4->Integral()<<endl;
+		cout<<"h5 "<<h5->Integral()<<endl;
+		cout<<"h6 "<<h6->Integral()<<endl;
+		cout<<"h7 "<<h7->Integral()<<endl;
+		cout<<"h8 "<<h8->Integral()<<endl;
+		cout<<"h9 "<<h9->Integral()<<endl;
+
+
+    	const double lumi = 24570 ; // pb-1
+
+		h1->Sumw2();
+		h2->Sumw2();
+		h3->Sumw2();
+		h4->Sumw2();
+		h5->Sumw2();
+		h6->Sumw2();
+		h7->Sumw2();
+		h8->Sumw2();
+		h9->Sumw2();
+
+		double h1_xs = 7475      ;
+		double h2_xs = 587.1     ;
+		double h3_xs = 167       ;
+		double h4_xs = 28.25     ;
+		double h5_xs = 8.195     ;
+		double h6_xs = 0.7346    ;
+		double h7_xs = 0.102     ;
+		double h8_xs = 0.00644   ;
+		double h9_xs = 0.000163  ;
+
+		double h1_nevent =2482816  ; //300
+		double h2_nevent =1998648  ; //470
+		double h3_nevent =1377400  ; //600
+		double h4_nevent =395328  ; //800
+		double h5_nevent =299967  ;  //1000 
+		double h6_nevent =38848  ; //1400
+		double h7_nevent =39975  ;  //1800
+		double h8_nevent =39990  ; //2400
+		double h9_nevent =39988  ; //3200
+
+		h1->Scale( h1_xs / h1_nevent * lumi);
+		h2->Scale( h2_xs / h2_nevent * lumi);
+		h3->Scale( h3_xs / h3_nevent * lumi);
+		h4->Scale( h4_xs / h4_nevent * lumi);
+		h5->Scale( h5_xs / h5_nevent * lumi);
+		h6->Scale( h6_xs / h6_nevent * lumi);
+		h7->Scale( h7_xs / h7_nevent * lumi);
+		h8->Scale( h8_xs / h8_nevent * lumi);
+		h9->Scale( h9_xs / h9_nevent * lumi);
+
+		h1->SetFillColor(99);
+		h2->SetFillColor(95);
+		h3->SetFillColor(90);
+		h4->SetFillColor(85);
+		h5->SetFillColor(75);
+		h6->SetFillColor(70);
+		h7->SetFillColor(65);
+		h8->SetFillColor(60);
+		h9->SetFillColor(55);
+
+		h1->SetLineColor(99);
+		h2->SetLineColor(95);
+		h3->SetLineColor(90);
+		h4->SetLineColor(85);
+		h5->SetLineColor(75);
+		h6->SetLineColor(70);
+		h7->SetLineColor(65);
+		h8->SetLineColor(60);
+		h9->SetLineColor(55);
+
+
+		// h1->Rebin(5);
+		// h2->Rebin(5);
+		// h3->Rebin(5);
+		// h4->Rebin(5);
+		// h5->Rebin(5);
+		// h6->Rebin(5);
+		// h7->Rebin(5);
+		// h8->Rebin(5);
+		// h9->Rebin(5);
+
+
+		cout<<"h1 "<<h1->Integral()<<endl;
+		cout<<"h2 "<<h2->Integral()<<endl;
+		cout<<"h3 "<<h3->Integral()<<endl;
+		cout<<"h4 "<<h4->Integral()<<endl;
+		cout<<"h5 "<<h5->Integral()<<endl;
+		cout<<"h6 "<<h6->Integral()<<endl;
+		cout<<"h7 "<<h7->Integral()<<endl;
+		cout<<"h8 "<<h8->Integral()<<endl;
+		cout<<"h9 "<<h9->Integral()<<endl;
+
+		TCanvas *c1 = new TCanvas("c1","",900,700);
+		THStack *hs = new THStack("hs","");
+		// THStack hs("hs","hs");
+  		  		hs->Add(h9);
+  		  		  		hs->Add(h8);
+  		hs->Add(h7);
+  		hs->Add(h6);
+  		hs->Add(h5);
+  		hs->Add(h4);
+  		hs->Add(h3);
+  		hs->Add(h2);
+
+  		hs->Add(h1);
+  		hs->SetMinimum(0.0001);
+		hs->Draw("HIST");
+
+		TLegend * leg;
+		leg  = new TLegend(0.7,0.54,0.95,0.89);
+		leg ->SetBorderSize(0);
+		leg ->SetFillColor(0);
+		leg ->SetFillStyle(0);
+		leg ->SetMargin(0.15);
+		leg ->AddEntry(h1, "300-470", "F");
+		leg ->AddEntry(h2, "470-600", "F");
+		leg ->AddEntry(h3, "600-800", "F");
+		leg ->AddEntry(h4, "800-1000", "F");
+		leg ->AddEntry(h5, "1000-1400", "F");
+		leg ->AddEntry(h6, "1400-1800", "F");
+		leg ->AddEntry(h7, "1800-2400", "F");
+		leg ->AddEntry(h8, "2400-3200", "F");
+		leg ->AddEntry(h9, "3200-Inf", "F");
+		leg ->Draw("same");
+
+		// h1->Draw();
+		// h2->Draw("same");
+		string savename = "testStack_"+hists_names[i]+".pdf";
+		string savename2 = "testStack_"+hists_names[i]+"_log.pdf";
+		c1->SaveAs(savename.c_str() );
+		c1->SetLogy();
+		c1->SaveAs(savename2.c_str() );
+		c1->SetLogy(0);
+
+  		h1->Add(h2);
+  		h1->Add(h3);
+  		h1->Add(h4);
+  		h1->Add(h5);
+  		h1->Add(h6);
+  		h1->Add(h7);
+  		h1->Add(h8);
+  		h1->Add(h9);
+        
+      	h1->SetName(hists_names[i].c_str());
+        h1->Write();
+    }
+    OutFile->Close();
+}
