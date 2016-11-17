1e735c6f3b868fe5f2cd9568eb4a1ff46626534d
James Dolen <jdolen@fnal.gov>
2016-11-17 12:50:09 -0600

    Add single top configs

---
+++ test/run_B2GTTbarTreeMaker_MC2_Toolbox.py
@@ -0,0 +1,280 @@
+#
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Ana")
+
+#----------------------------------------------------------------------------------------
+### SETUP
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load("Configuration.EventContent.EventContent_cff")
+process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+process.GlobalTag.globaltag = '80X_mcRun2_asymptotic_2016_miniAODv2_v1'
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
+process.options.allowUnscheduled = cms.untracked.bool(True)
+
+isMC   = True
+
+
+#----------------------------------------------------------------------------------------
+### INPUT
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(2000) )
+process.MessageLogger.cerr.FwkReport.reportEvery = 10000
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+       'root://cms-xrd-global.cern.ch//store/mc/RunIISpring16MiniAODv2/RSGluonToTT_M-1500_TuneCUETP8M1_13TeV-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/70000/5A55B211-4938-E611-90DE-F832E4CC4EB1.root'
+      #'file:root://cms-xrd-global.cern.ch//store/mc/RunIISpring16MiniAODv2/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14_ext3-v1/00000/0064B539-803A-E611-BDEA-002590D0B060.root',
+      #'file:root://cms-xrd-global.cern.ch//store/mc/RunIISpring16MiniAODv2/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14_ext3-v1/00000/008796C3-B53A-E611-8853-0025905C42A6.root',
+      #'file:root://cms-xrd-global.cern.ch//store/mc/RunIISpring16MiniAODv2/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14_ext3-v1/00000/00E165A2-C63A-E611-AA23-141877344134.root',
+      #'file:root://cms-xrd-global.cern.ch//store/mc/RunIISpring16MiniAODv2/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14_ext3-v1/00000/00FBAFC2-B53A-E611-9F5D-0025904C641C.root',
+      #'file:root://cms-xrd-global.cern.ch//store/mc/RunIISpring16MiniAODv2/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14_ext3-v1/00000/02098436-CC3A-E611-8C96-B083FED3EE25.root',
+      #'file:root://cms-xrd-global.cern.ch//store/mc/RunIISpring16MiniAODv2/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14_ext3-v1/00000/0241AA22-AA3A-E611-9ECD-002590D9D9FC.root',
+      #'file:root://cms-xrd-global.cern.ch//store/mc/RunIISpring16MiniAODv2/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14_ext3-v1/00000/024D71B6-703A-E611-ADA0-0090FAA576C0.root',
+      #'file:root://cms-xrd-global.cern.ch//store/mc/RunIISpring16MiniAODv2/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14_ext3-v1/00000/028CEA43-D03A-E611-94F3-141877411D83.root',
+      #'file:root://cms-xrd-global.cern.ch//store/mc/RunIISpring16MiniAODv2/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14_ext3-v1/00000/0405AA62-E03A-E611-988D-C81F66B7ED99.root',
+      #'file:root://cms-xrd-global.cern.ch//store/mc/RunIISpring16MiniAODv2/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14_ext3-v1/00000/046EAFC4-B53A-E611-8B66-0025905C2CD0.root',
+      #'file:root://cms-xrd-global.cern.ch//store/mc/RunIISpring16MiniAODv2/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14_ext3-v1/00000/048BDC26-B93A-E611-9CAF-0025905C4264.root'
+      #'file:root://cmsxrootd.fnal.gov///store/user/jdolen/B2G2016/ZprimeToTT_M-3000_W-30_TuneCUETP8M1_13TeV-madgraphMLM_RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_MINIAOD.root'
+      #'file:root://cmsxrootd.fnal.gov//store/mc/RunIISpring16MiniAODv2/ZprimeToTT_M-2000_W-20_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/90000/68594422-C039-E611-91D0-842B2B17F73D.root',
+      #'file:root://cmsxrootd.fnal.gov//store/mc/RunIISpring16MiniAODv2/ZprimeToTT_M-2000_W-20_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/90000/78371B76-D439-E611-896B-B083FECF837B.root',
+      #'file:root://cmsxrootd.fnal.gov//store/mc/RunIISpring16MiniAODv2/ZprimeToTT_M-2000_W-20_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/90000/847453B3-F739-E611-909C-90B11C0BCBC2.root',
+      #'file:root://cmsxrootd.fnal.gov//store/mc/RunIISpring16MiniAODv2/ZprimeToTT_M-2000_W-20_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/90000/AA4C714B-B239-E611-8CF5-782BCB63EBF5.root',
+      #'file:root://cmsxrootd.fnal.gov//store/mc/RunIISpring16MiniAODv2/ZprimeToTT_M-2000_W-20_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/90000/D85CC004-F839-E611-A153-B083FED429D6.root'    
+    )
+)
+
+#----------------------------------------------------------------------------------------
+### MET Filters
+process.load('RecoMET.METFilters.BadChargedCandidateFilter_cfi')
+process.load('RecoMET.METFilters.BadPFMuonFilter_cfi')
+process.BadChargedCandidateFilter.muons = cms.InputTag("slimmedMuons")
+process.BadChargedCandidateFilter.PFCandidates = cms.InputTag("packedPFCandidates")
+process.BadChargedCandidateFilter.debug = cms.bool(False)
+process.BadPFMuonFilter.muons = cms.InputTag("slimmedMuons")
+process.BadPFMuonFilter.PFCandidates = cms.InputTag("packedPFCandidates")
+process.BadPFMuonFilter.debug = cms.bool(False)
+
+
+#----------------------------------------------------------------------------------------
+### VID
+from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
+dataFormat = DataFormat.MiniAOD
+switchOnVIDElectronIdProducer(process, dataFormat)
+my_id_modules = [
+    'RecoEgamma.ElectronIdentification.Identification.cutBasedElectronHLTPreselecition_Summer16_V1_cff',
+    'RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_Summer16_80X_V1_cff',
+    'RecoEgamma.ElectronIdentification.Identification.heepElectronID_HEEPV60_cff'
+]
+
+for idmod in my_id_modules:
+    setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
+
+#----------------------------------------------------------------------------------------
+### Puppi (https://twiki.cern.ch/twiki/bin/viewauth/CMS/PUPPI)
+#process.load('CommonTools/PileupAlgos/Puppi_cff')
+#process.puppi.candName = cms.InputTag('packedPFCandidates')
+#process.puppi.vertexName = cms.InputTag('offlineSlimmedPrimaryVertices')
+#process.puppi.useExistingWeights = cms.bool(True)
+#process.puppiOnTheFly = process.puppi.clone()
+#process.puppiOnTheFly.useExistingWeights = True
+
+
+#----------------------------------------------------------------------------------------
+### MET   //https://twiki.cern.ch/twiki/bin/view/CMS/MissingETUncertaintyPrescription
+from PhysicsTools.PatUtils.tools.runMETCorrectionsAndUncertainties import runMetCorAndUncFromMiniAOD
+
+# If you only want to re-correct and get the proper uncertainties
+runMetCorAndUncFromMiniAOD(process,
+                       isData=not isMC,
+                       )
+
+# If you would like to re-cluster and get the proper uncertainties
+# runMetCorAndUncFromMiniAOD(process,
+#                        isData=True (or False),
+#                        pfCandColl=cms.InputTag("packedPFCandidates"),
+#                        recoMetFromPFCs=True,
+#                        )
+
+#----------------------------------------------------------------------------------------
+### Toolbox (https://twiki.cern.ch/twiki/bin/viewauth/CMS/JetToolbox)
+from JMEAnalysis.JetToolbox.jetToolbox_cff import jetToolbox
+
+ak8Cut = 'pt > 200 && abs(eta) < 2.5'
+ak8pupCut = 'pt > 150 && abs(eta) < 2.5'
+
+listBTagInfos = [
+     'pfInclusiveSecondaryVertexFinderTagInfos',
+]
+listBtagDiscriminatorsAK8 = [ 
+    'pfJetProbabilityBJetTags',
+    'pfCombinedInclusiveSecondaryVertexV2BJetTags',
+    'pfCombinedMVAV2BJetTags',
+    # 'pfCombinedCvsLJetTags',
+    # 'pfCombinedCvsBJetTags',
+    'pfBoostedDoubleSecondaryVertexAK8BJetTags',
+    # 'pfBoostedDoubleSecondaryVertexCA15BJetTags',
+]
+
+# |---- jetToolBox: JETTOOLBOX RUNNING ON MiniAOD FOR AK8 JETS USING CHS
+# |---- jetToolBox: Applying this corrections: ('AK8PFchs', ['L1FastJet', 'L2Relative', 'L3Absolute'], 'None')
+# |---- jetToolBox: Running ak8PFJetsCHSSoftDropMass, selectedPatJetsAK8PFCHSSoftDropPacked, selectedPatJetsAK8PFCHSSoftDropSubjets, ak8PFJetsCHSPrunedMass, ak8PFJetsCHSTrimmedMass, ak8PFJetsCHSFilteredMass, NjettinessAK8CHS, NsubjettinessAK8PFCHSSoftDropSubjets.
+# |---- jetToolBox: Creating selectedPatJetsAK8PFCHS collection.
+# vector<pat::Jet>                      "selectedPatJetsAK8PFCHS"   ""               "Ana"     
+# vector<pat::Jet>                      "selectedPatJetsAK8PFCHSSoftDropPacked"   ""               "Ana"     
+# vector<pat::Jet>                      "selectedPatJetsAK8PFCHSSoftDropPacked"   "SubJets"        "Ana"     
+# vector<reco::GenJet>                  "selectedPatJetsAK8PFCHS"   "genJets"        "Ana"     
+# vector<reco::PFCandidate>             "selectedPatJetsAK8PFCHS"   "pfCandidates"   "Ana"    
+
+jetToolbox( process, 'ak8', 'ak8JetSubs', 'out', 
+  runOnMC = isMC, 
+  PUMethod='CHS', 
+  # updateCollection='slimmedJetsAK8', # can't run groomers on this yet
+  # JETCorrPayload='AK8PFchs', # needed for update collection
+  addSoftDropSubjets = True, 
+  addTrimming = True, rFiltTrim=0.2, ptFrac=0.05,
+  addPruning = True, 
+  addFiltering = True, 
+  addSoftDrop = True, 
+  addNsub = True, 
+  bTagInfos = listBTagInfos, 
+  bTagDiscriminators = listBtagDiscriminatorsAK8, 
+  addCMSTopTagger = False, 
+  Cut = ak8Cut , 
+  addNsubSubjets = True, 
+  subjetMaxTau = 4 )
+
+
+# |---- jetToolBox: JETTOOLBOX RUNNING ON MiniAOD FOR AK8 JETS USING Puppi
+# |---- jetToolBox: Applying this corrections: ('AK8PFPuppi', ['L2Relative', 'L3Absolute'], 'None')
+# |---- jetToolBox: Running ak8PFJetsPuppiSoftDropMass, selectedPatJetsAK8PFPuppiSoftDropPacked, selectedPatJetsAK8PFPuppiSoftDropSubjets, ak8PFJetsPuppiPrunedMass, ak8PFJetsPuppiTrimmedMass, ak8PFJetsPuppiFilteredMass, NjettinessAK8Puppi, NsubjettinessAK8PFPuppiSoftDropSubjets.
+# |---- jetToolBox: Creating selectedPatJetsAK8PFPuppi collection.
+# vector<pat::Jet>                      "selectedPatJetsAK8PFPuppi"   ""               "Ana"     
+# vector<pat::Jet>                      "selectedPatJetsAK8PFPuppiSoftDropPacked"   ""               "Ana"  
+# vector<pat::Jet>                      "selectedPatJetsAK8PFPuppiSoftDropPacked"   "SubJets"        "Ana"     
+# vector<reco::GenJet>                  "selectedPatJetsAK8PFPuppi"   "genJets"        "Ana"     
+# vector<reco::PFCandidate>             "selectedPatJetsAK8PFPuppi"   "pfCandidates"   "Ana"     
+
+
+jetToolbox( process, 'ak8', 'ak8JetSubs', 'out', 
+  runOnMC = isMC, 
+  PUMethod='Puppi', 
+  addSoftDropSubjets = True, 
+  addTrimming = True,  rFiltTrim=0.2, ptFrac=0.05,
+  addPruning = True, 
+  addFiltering = True, 
+  addSoftDrop = True, 
+  addNsub = True, 
+  bTagInfos = listBTagInfos, 
+  bTagDiscriminators = listBtagDiscriminatorsAK8, 
+  addCMSTopTagger = False, 
+  Cut = ak8pupCut , 
+  addNsubSubjets = True, 
+  subjetMaxTau = 4 )
+
+#----------------------------------------------------------------------------------------
+### Analyzer
+
+process.ana = cms.EDAnalyzer('B2GTTbarTreeMaker',
+    useToolbox    = cms.bool(True),
+    verbose       = cms.bool(False),
+    verboseGen    = cms.bool(False),
+    runGenLoop    = cms.bool(True),
+    isZprime      = cms.bool(False),
+    isttbar       = cms.bool(False),
+    isRSG         = cms.bool(False),
+    ak8chsInput          = cms.InputTag("selectedPatJetsAK8PFCHS"),   
+    ak8puppiInput        = cms.InputTag("selectedPatJetsAK8PFPuppi"),
+    ak8chsSubjetsInput   = cms.InputTag("selectedPatJetsAK8PFCHSSoftDropPacked","SubJets"),
+    ak8puppiSubjetsInput = cms.InputTag("selectedPatJetsAK8PFPuppiSoftDropPacked","SubJets"),
+    triggerBits          = cms.InputTag("TriggerResults", "", "HLT"),
+    lheSrc               = cms.InputTag("externalLHEProducer", "", "LHE"),
+    eleIdFullInfoMapToken_HLTpre  = cms.InputTag("egmGsfElectronIDs:cutBasedElectronHLTPreselection-Summer16-V1"),
+    eleIdFullInfoMapToken_Loose   = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Summer16-80X-V1-loose"),
+    eleIdFullInfoMapToken_Medium  = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Summer16-80X-V1-medium"),
+    eleIdFullInfoMapToken_Tight   = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Summer16-80X-V1-tight"),
+    eleIdFullInfoMapToken_HEEP    = cms.InputTag("egmGsfElectronIDs:heepElectronID-HEEPV60"), 
+    jecPayloadsAK8chs = cms.vstring([
+                                    'Spring16_25nsV6_MC_L1FastJet_AK8PFchs.txt',
+                                    'Spring16_25nsV6_MC_L2Relative_AK8PFchs.txt',
+                                    'Spring16_25nsV6_MC_L3Absolute_AK8PFchs.txt',
+                                    'Spring16_25nsV6_MC_L2L3Residual_AK8PFchs.txt',
+                                    'Spring16_25nsV6_MC_Uncertainty_AK8PFchs.txt'
+                                    ]),
+    jecPayloadsAK4chs = cms.vstring([
+                                    'Spring16_25nsV6_MC_L1FastJet_AK4PFchs.txt',
+                                    'Spring16_25nsV6_MC_L2Relative_AK4PFchs.txt',
+                                    'Spring16_25nsV6_MC_L3Absolute_AK4PFchs.txt',
+                                    'Spring16_25nsV6_MC_L2L3Residual_AK4PFchs.txt',
+                                    'Spring16_25nsV6_MC_Uncertainty_AK4PFchs.txt'
+                                    ]),
+    jecPayloadsAK8pup = cms.vstring([
+                                    'Spring16_25nsV6_MC_L1FastJet_AK8PFPuppi.txt',
+                                    'Spring16_25nsV6_MC_L2Relative_AK8PFPuppi.txt',
+                                    'Spring16_25nsV6_MC_L3Absolute_AK8PFPuppi.txt',
+                                    'Spring16_25nsV6_MC_L2L3Residual_AK8PFPuppi.txt',
+                                    'Spring16_25nsV6_MC_Uncertainty_AK8PFPuppi.txt'
+                                    ]),
+    jecPayloadsAK4pup = cms.vstring([
+                                    'Spring16_25nsV6_MC_L1FastJet_AK4PFPuppi.txt',
+                                    'Spring16_25nsV6_MC_L2Relative_AK4PFPuppi.txt',
+                                    'Spring16_25nsV6_MC_L3Absolute_AK4PFPuppi.txt',
+                                    'Spring16_25nsV6_MC_L2L3Residual_AK4PFPuppi.txt',
+                                    'Spring16_25nsV6_MC_Uncertainty_AK4PFPuppi.txt'
+                                    ]),
+    jerSFtext         = cms.string('Spring16_25nsV6_MC_SF_AK8PFchs.txt'
+                                    )
+)
+
+
+#----------------------------------------------------------------------------------------
+### Out
+
+# If you want to output the newly recoconstruted jets
+# process.out = cms.OutputModule(
+#     "PoolOutputModule",
+#     fileName = cms.untracked.string('tool.root'),
+#     outputCommands = cms.untracked.vstring(
+#       "keep *_selectedPatJetsAK8PFCHS_*_*",
+#       "keep *_selectedPatJetsAK8PFCHSSoftDropPacked_*_*",
+#       "keep *_selectedPatJetsAK8PFPuppi_*_*",
+#       "keep *_selectedPatJetsAK8PFPuppiSoftDropPacked_*_*"
+#       )
+#     )
+# process.endpath = cms.EndPath(process.out) 
+
+
+process.TFileService = cms.Service("TFileService",
+      fileName = cms.string("tree.root"),
+      closeFileFast = cms.untracked.bool(True)
+  )
+
+
+#Adding SimpleMemoryCheck service:
+#process.SimpleMemoryCheck=cms.Service("SimpleMemoryCheck",
+#                                   ignoreTotal=cms.untracked.int32(0),
+#                                   oncePerEventMode=cms.untracked.bool(True)
+#)
+
+#process.Timing = cms.Service("Timing"
+##    ,summaryOnly = cms.untracked.bool(True)
+#)
+
+#process.ProfilerService = cms.Service("ProfilerService",
+#    lastEvent = cms.untracked.int32(10),
+#    firstEvent = cms.untracked.int32(1),
+#    paths = cms.untracked.vstring('p')
+#)
+ 
+
+process.p = cms.Path(
+  process.BadChargedCandidateFilter*
+  process.BadPFMuonFilter*
+  process.egmGsfElectronIDSequence*
+  process.fullPatMetSequence *
+  process.ana
+)
+
